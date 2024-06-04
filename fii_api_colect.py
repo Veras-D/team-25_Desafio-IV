33af657e304b6118b4066ca14612474bdfe85584
Veras-D <dveras2310@gmail.com>
2024-06-04 09:07:16 -0300

    Update tasks

--- fii_api_colect.py
+++ fii_api_colect.py
@@ -1,7 +1,8 @@
 import scrapy
 import json
+from scrapy.crawler import CrawlerProcess
 
-headers = {"X-Funds-Nonce": "61495f60b533cc40ad822e054998a3190ea9bca0d94791a1da"}  # Update with valid value
+headers = {"X-Funds-Nonce": "61495f60b533cc40ad822e054998a3190ea9bca0d94791a1da"}
 
 class FiispiderSpider(scrapy.Spider):
     name = "fiispider"
@@ -9,6 +10,7 @@ class FiispiderSpider(scrapy.Spider):
 
     def start_requests(self):
         start_urls = ["https://www.fundsexplorer.com.br/wp-json/funds/v1/get-ranking"]
+        # https://www.fundsexplorer.com.br/wp-json/funds/v1/get-properties
         for url in start_urls:
             yield scrapy.Request(url, headers=headers)
 
@@ -17,7 +19,6 @@ class FiispiderSpider(scrapy.Spider):
             yield row
 
 # Run the spider
-from scrapy.crawler import CrawlerProcess
 
 process = CrawlerProcess()
 process.crawl(FiispiderSpider)
