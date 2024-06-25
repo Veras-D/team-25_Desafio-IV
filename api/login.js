fee38fcf0c3e338740a927191d2fd3de2a936e60
Veras-D <dveras2310@gmail.com>
2024-06-25 20:49:31 -0300

    testing api

---
+++ api/login.js
@@ -0,0 +1,33 @@
+// api/login.js
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+router.post('/', async (req, res) => {
+    const { email, senha } = req.body;
+
+    if (!email || !senha) {
+        return res.status(422).json({ message: 'E-mail e senha são obrigatórios' });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(senha, user.senha);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Senha incorreta' });
+        }
+
+        res.status(200).redirect('/Page/home/home.html');
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+module.exports = router;
