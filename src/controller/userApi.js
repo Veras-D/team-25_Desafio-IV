4bcc9fc3b79c55ac8284da230dd4fc4efb1ad7d3
Veras-D <dveras2310@gmail.com>
2024-06-25 21:51:03 -0300

    update

--- src/controller/userApi.js
+++ src/controller/userApi.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const bcrypt = require('bcrypt'); // Para verificar a senha hash
+const bcrypt = require('bcrypt');
 
 // Rota de cadastro
 router.post('/users', async (req, res) => {
--- src/index.js
+++ src/index.js
@@ -5,32 +5,25 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 const app = express();
-
-// Configurando o Express para servir arquivos estáticos da pasta "Page"
-app.use(express.static(path.join(__dirname, 'Page')));
-
-// Configurando o Body-Parser para lidar com dados JSON e codificados em URL
+app.use(express.static(path.join(__dirname, '..', 'Page')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Configurando o MongoDB
 const url_db = "mongodb+srv://pgsilva2002:_trilhas_inova_desafio_25@cluster0.nsscd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.set('strictQuery', true);
 mongoose.connect(url_db)
-    .then(() => {
-        console.log('Connected to DB');
-    })
-    .catch(error => {
-        console.error('Connection to DB Failed');
-        console.error(error.message);
-        process.exit(-1);
-    });
+  .then(() => {
+    console.log('Connected to DB');
+  })
+  .catch(error => {
+    console.error('Connection to DB Failed');
+    console.error(error.message);
+    process.exit(-1);
+  });
 
-// Importando e utilizando as rotas de API
-app.use('/api/users', require('../api/users'));
-app.use('/api/login', require('../api/login'));
+const userApi = require('./controller/userApi');
+app.use('/api', userApi);
 
-// Iniciando o servidor
 app.listen(port, () => {
-    console.log('SERVER IS UP');
+  console.log(`SERVER IS UP on port ${port}`);
 });
