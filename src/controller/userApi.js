ac8529a76dce69986b77b451b91fa79660f5c064
Veras-D <dveras2310@gmail.com>
2024-06-26 22:29:58 -0300

    solving path

--- src/controller/userApi.js
+++ src/controller/userApi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./src/models/User');
+const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
